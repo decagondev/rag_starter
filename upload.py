d248ab7973202961492a7e22e4a350fbaef8f5a1
Tom Tarpey <tomtarpeydev@gmail.com>
2024-05-26 05:10:39 +0100

    prep-documents-for-upload

--- upload.py
+++ upload.py
@@ -5,6 +5,8 @@ from langchain_community.document_loaders.pdf import PyPDFLoader
 from langchain_community.document_loaders import DirectoryLoader
 
 # Prep documents to be uploaded to the vector database (Pinecone)
+loader = DirectoryLoader('./', glob="**/*.pdf", loader_cls=PyPDFLoader)
+raw_docs = loader.load()
 
 # Split documents into smaller chunks
 
